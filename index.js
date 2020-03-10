82348a5b04ea2cb07678d811094aae1f34f1d4c1
amin havasi <pau.ahq@gmail.com>
2020-03-10 22:11:57 +0330

    env added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
+require('dotenv').config();
 
-http.listen();
+http.listen(process.env.PORT, () => {
+    console.log(`server is running on port ${process.env.PORT}`);
+});
