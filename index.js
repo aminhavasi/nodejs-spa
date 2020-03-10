85dfc07c46fc1f4a88fb770331ca8f687f086117
amin havasi <pau.ahq@gmail.com>
2020-03-10 22:13:06 +0330

    http  header for secure

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 require('dotenv').config();
+const helmet = require('helmet');
+app.use(helmet());
 
 http.listen(process.env.PORT, () => {
     console.log(`server is running on port ${process.env.PORT}`);
