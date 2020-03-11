44541922428b32d64bbefbd821059d6b3000abc1
amin havasi <pau.ahq@gmail.com>
2020-03-11 19:02:26 +0330

    clear user

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ mongoose.connect(process.env.URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-app.use(require('./src/routes/user'));
 http.listen(process.env.PORT, () => {
     console.log(`server is running on port ${process.env.PORT}`);
 });
