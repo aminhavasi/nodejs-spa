acd65e59ad44a782accd8d3b5000610655a441a3
amin havasi <pau.ahq@gmail.com>
2020-03-11 23:42:28 +0330

    login change

--- src/routes/login.js
+++ src/routes/login.js
@@ -18,23 +18,6 @@ router.post('/', async (req, res) => {
         res.send('pl');
     }
 });
-router.post('/sign', async (req, res) => {
-    try {
-        const { error } = validator(req.body);
-
-        if (error) return res.status(400).send(error.details[0].message);
-
-        let user = await User.findOne({ email: req.body.email });
-        if (user) return res.status(400).send('user already registred');
-        user = new User(_.pick(req.body, ['name', 'email', 'password']));
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(req.body.password, salt);
-        await user.save();
-        res.status(200).send(_.pick(user, ['_id', 'name', 'email']));
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
 
 const validationLogin = req => {
     const shema = Joi.object({
